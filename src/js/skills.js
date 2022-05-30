7ee1d92970fa5d496174c24d205d80f2939e839a
Nicolas Magno <nicolasmagno.100@gmail.com>
2022-05-30 10:46:20 -0300

    feat: adding assets and improving design concepts

--- src/js/skills.js
+++ src/js/skills.js
@@ -9,31 +9,31 @@ export const skills = () => {
 
     menuContainer.innerHTML = `
     <div class="menu__item">
-      <img src="./src/assets/html.png" alt="Html Icon">
+      <img src="./src/assets/html.png" loading="eager" alt="Html Icon">
     </div>
     <div class="menu__item">
-      <img src="./src/assets/css.png" alt="Css Icon">
+      <img src="./src/assets/css.png" loading="eager" alt="Css Icon">
     </div>
     <div class="menu__item">
-      <img src="./src/assets/javascript.png" alt="Javascript Icon">
+      <img src="./src/assets/javascript.png" loading="eager" alt="Javascript Icon">
     </div>
     <div class="menu__item">
-      <img src="./src/assets/sass.png" alt="Sass Icon">
+      <img src="./src/assets/sass.png" loading="eager" alt="Sass Icon">
     </div>
     <div class="menu__item">
-      <img src="./src/assets/react.jpg" alt="React Icon">
+      <img src="./src/assets/react.jpg" loading="eager" alt="React Icon">
     </div>
     <div class="menu__item">
-      <img src="./src/assets/firebase.png" alt="Firebase Icon">
+      <img src="./src/assets/firebase.png" loading="eager" alt="Firebase Icon">
     </div>
     <div class="menu__item">
-      <img src="./src/assets/node.png" alt="Node Icon">
+      <img src="./src/assets/node.png" loading="eager" alt="Node Icon">
     </div>
     <div class="menu__item">
-      <img src="./src/assets/git.png" alt="Git Icon">
+      <img src="./src/assets/git.png" loading="eager" alt="Git Icon">
     </div>
     <div class="menu__item">
-      <img src="./src/assets/figma.png" alt="Figma Icon">
+      <img src="./src/assets/figma.png" loading="eager" alt="Figma Icon">
     </div>
     <button class="menu__item back">Back</button>
       `;
