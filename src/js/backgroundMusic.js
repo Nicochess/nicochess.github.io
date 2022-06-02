c0b7910f36259331b115580e6215bc1908b9f72f
Nicolas <66505477+Nicochess@users.noreply.github.com>
2022-06-02 15:33:07 -0300

    fix: music volume and style improving

--- src/js/backgroundMusic.js
+++ src/js/backgroundMusic.js
@@ -4,10 +4,10 @@ const timeNow = new Date().getHours();
 
 export const backgroundMusic = () => {
   if (timeNow > 16 || timeNow < 6) {
-    playAudio("./src/assets/dawmmusic.mp3", 0.1, true);
+    playAudio("./src/assets/dawmmusic.mp3", 0.05, true);
   } else if (timeNow < 9) {
-    playAudio("./src/assets/sunrisemusic.mp3", 0.1, true);
+    playAudio("./src/assets/sunrisemusic.mp3", 0.05, true);
   } else {
-    playAudio("./src/assets/morningmusic.mp3", 0.1, true);
+    playAudio("./src/assets/morningmusic.mp3", 0.05, true);
   }
 };
